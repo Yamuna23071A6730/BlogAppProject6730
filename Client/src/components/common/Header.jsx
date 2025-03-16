763f0105f7ec38f4084248dc56389bcb41c33f50
Yamuna23071A6730 <151129678+Yamuna23071A6730@users.noreply.github.com>
2025-03-16 14:08:12 +0530

    Update Header.jsx

--- Client/src/components/common/Header.jsx
+++ Client/src/components/common/Header.jsx
@@ -5,22 +5,19 @@ import { useClerk, useUser } from '@clerk/clerk-react'
 import { userAuthorContextObj } from '../../contexts/UserAuthorContext'
 
 const Header = () => {
-  //const { userId } = useAuth();
   const { signOut } = useClerk();
-  const { currentUser,setCurrentUser } = useContext(userAuthorContextObj);
- // console.log(currentUser);
+  const { currentUser, setCurrentUser } = useContext(userAuthorContextObj);
 
- const navigate=useNavigate()
-  // Add these lines
+  const navigate = useNavigate();
   const { isSignedIn, user, isLoaded } = useUser();
+
   const handleSignOut = async () => {
     console.log("signout called")
     try {
       await signOut();
-      // Clear local storage after successful sign out
-      setCurrentUser(null)
+      setCurrentUser(null);
       localStorage.clear();
-      navigate('/')
+      navigate('/');
     } catch (err) {
       console.error("Error signing out:", err);
     }
@@ -28,49 +25,38 @@ const Header = () => {
 
   return (
     <div>
-      <nav className="header   d-flex justify-content-between align-content-center py-3">
+      <nav className="header d-flex justify-content-between align-content-center py-3">
         <div className="d-flex justify-content-center">
-          <Link href="/">
-            <img src={logo} alt="" width="60px" className="ms-4" />
+          <Link to="/">
+            <img src={logo} alt="Blog Logo" width="60px" className="ms-4" />
           </Link>
+          <h2 className="ms-3 text-white">InspireSphere</h2> {/* Added title */}
         </div>
-        <ul className="text-white d-flex justify-content-center  list-unstyled">
+        <ul className="text-white d-flex justify-content-center list-unstyled">
           {!isSignedIn ? (
             <>
               <li>
-                <Link to="signin" className=" link me-4 ">
+                <Link to="signin" className="link me-4">
                   Sign In
                 </Link>
               </li>
               <li>
-                <Link to="signup" className="link  me-4 ">
+                <Link to="signup" className="link me-4">
                   Sign Up
                 </Link>
               </li>
             </>
           ) : (
-            <div
-              className="d-flex justify-content-around"
-              style={{ width: "200px" }}
-            >
+            <div className="d-flex justify-content-around" style={{ width: "200px" }}>
               <div className="user-button">
-                <div style={{position:"relative"}}>
-                  <img
-                    src={user.imageUrl}
-                    width="40px"
-                    className="rounded-circle"
-                    alt=""
-                    
-                  />
-                  <p className="role" style={{position:"absolute", top:"0px",right:"-20px"}}>{currentUser.role}</p>
+                <div style={{ position: "relative" }}>
+                  <img src={user.imageUrl} width="40px" className="rounded-circle" alt="User" />
+                  <p className="role" style={{ position: "absolute", top: "0px", right: "-20px" }}>
+                    {currentUser.role}
+                  </p>
                 </div>
-                <p className="mb-0 user-name"> {user.firstName}</p>
-                {/* <div >
-                  {currentUser.role}
-                </div> */}
+                <p className="mb-0 user-name">{user.firstName}</p>
               </div>
-
-              {/* <UserButton /> */}
               <button onClick={handleSignOut} className="signout-btn">
                 Signout
               </button>
@@ -82,4 +68,4 @@ const Header = () => {
   );
 };
 
-export default Header
+export default Header;
